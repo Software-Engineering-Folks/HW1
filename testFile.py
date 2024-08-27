c8759b6a67aaf4033dc26eb6ff36c5b0c1133e57
Devang Saraogi <devangsaraogi@outlook.com>
2024-08-27 20:40:15 +0000

    changes to testFile.py

--- testFile.py
+++ testFile.py
@@ -6,5 +6,5 @@ def test_successful():
 def test_successful():
     assert mf.bubblesort([1,5,0,3]) == [0,1,3,5]
 
-# def test_failed():
-#     assert mf.bubblesort([1,6,5,3]) == [1,5,3,6]
+def test_failed():
+    assert mf.bubblesort([1,6,5,3]) == [1,5,3,6]
