2e91844666eb327ada3800bd8dc30b3a88b7543a
snehilbehar <54533167+snehilbehar@users.noreply.github.com>
2024-09-10 18:22:59 +0000

    Debugging done

--- HW2/hw2_debugging.py
+++ HW2/hw2_debugging.py
@@ -1,7 +1,13 @@
+'''This module has two function - 
+1. merge_sort(arr) - Takes an array as input and performs merge sort on it
+2. recombine(left_array, right_array) - This takes two sorted input arrays 
+and merges them in a sorted fashion'''
+
 import rand
 
 
 def merge_sort(arr_func):
+    '''This function performs a recursive merge sort to the array input passed'''
     if len(arr_func) == 1:
         return arr_func
 
@@ -11,27 +17,29 @@ def merge_sort(arr_func):
 
 
 def recombine(left_arr, right_arr):
-    left_index = 0
-    right_index = 0
-    merge_arr = [None] * (len(left_arr) + len(right_arr))
+    '''This function performs the merging of the two sorted input arrays 
+    left_array and right_array in a sorted fashion'''
+
+    left_index, right_index, merge_arr = 0, 0, []
+
     while left_index < len(left_arr) and right_index < len(right_arr):
         if left_arr[left_index] < right_arr[right_index]:
-            right_index += 1
-            merge_arr[left_index + right_index] = left_arr[left_index]
-        else:
+            merge_arr.append(left_arr[left_index])
             left_index += 1
-            merge_arr[left_index + right_index] = right_arr[right_index]
+        else:
+            merge_arr.append(right_arr[right_index])
+            right_index += 1
 
     for i in range(right_index, len(right_arr)):
-        merge_arr[left_index + right_index] = right_arr[i]
+        merge_arr.append(right_arr[i])
 
     for i in range(left_index, len(left_arr)):
-        merge_arr[left_index + right_index] = left_arr[i]
+        merge_arr.append(left_arr[i])
 
     return merge_arr
 
 
-arr = rand.random_array([None] * 20)
-arr_out = merge_sort(arr)
+array = rand.random_array([None] * 20)
+arr_out = merge_sort(array)
 
 print(arr_out)
