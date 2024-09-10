91f57449d083568e474912983aca5124c5eb6085
snehilbehar <54533167+snehilbehar@users.noreply.github.com>
2024-09-10 18:31:15 +0000

    Rand.py lint fixed

--- HW2/rand.py
+++ HW2/rand.py
@@ -3,10 +3,12 @@ import subprocess
 
 
 def random_array(arr):
-    '''This function creates an array of random integers. The length of the array is the same as the length of the input array.'''
+    '''This function creates an array of random integers. 
+    The length of the array is the same as the length of the input array.'''
+
     shuffled_num = None
-    for i in range(len(arr)):
-        shuffled_num = subprocess.run(
-            ["shuf", "-i1-20", "-n1"], capture_output=True, check='True')
-        arr[i] = int(shuffled_num.stdout)
+
+    for idx in enumerate(arr):
+        shuffled_num = subprocess.run(["shuf", "-i1-20", "-n1"], capture_output=True, check='True')
+        arr[idx] = int(shuffled_num.stdout)
     return arr
