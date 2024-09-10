ff87b9a78faffed8508b3035bc8a76e5bdbdff8f
snehilbehar <54533167+snehilbehar@users.noreply.github.com>
2024-09-10 20:01:08 +0000

    Remove failing test

--- testFile.py
+++ testFile.py
@@ -3,6 +3,6 @@ import src.myfile as mf
 def test_successful():
     assert mf.bubblesort([1,6,5,3]) == [1,3,5,6]
 
-def test_failed():
-    assert mf.bubblesort([1,6,5,3]) == [1,5,3,6]
+# def test_failed():
+#     assert mf.bubblesort([1,6,5,3]) == [1,5,3,6]
 
