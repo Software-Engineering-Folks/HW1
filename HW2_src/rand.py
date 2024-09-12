f592b4a32b808ca5874d8ef98c8c5caf98233ccd
snehilbehar <54533167+snehilbehar@users.noreply.github.com>
2024-09-12 20:15:00 +0000

    Fixeds to rand and tests

--- HW2_src/rand.py
+++ HW2_src/rand.py
@@ -1,13 +1,10 @@
 '''This module has a docstring. It's right here. You are looking at it right now.'''
-import subprocess
+import secrets
 
 def random_array(arr):
     '''This function creates an array of random integers. 
     The length of the array is the same as the length of the input array.'''
 
-    shuffled_num = None
-
     for enum_array in enumerate(arr):
-        shuffled_num = subprocess.run(["shuf", "-i1-20", "-n1"], capture_output=True, check='True')
-        arr[enum_array[0]] = int(shuffled_num.stdout)
+        arr[enum_array[0]] = secrets.randbelow(20) + 1
     return arr
