a3a1fcdc3aa4f12e0e1f843a9a0f54adf11f56f6
Devang Saraogi <devangsaraogi@outlook.com>
2024-10-11 02:19:53 +0000

    correction in task2 script

--- HW4_src/task2.sh
+++ HW4_src/task2.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-grep -l "sample" dataset/file* | xargs -I {} sh -c 'echo "{} $(grep -o "CSC510" "{}" | wc -l) $(wc -c < "{}")"' | awk '$2 >= 3' | sort -k2,2nr -k3,3nr | awk '{print $1, $2}' | sed 's/file_/filtered_/'
+grep -l "sample" dataset1/file* | 
+xargs -I {} sh -c 'echo "{} $(grep -o "CSC510" "{}" | wc -l) $(wc -c < "{}")"' | 
+awk '$2 >= 3' |
+sort -k2,2nr -k3,3nr |
+awk '{print $1, $2}' |
+sed 's/file_/filtered_/'
